d99d68f92b8ba7034d618f745ce87af89de6891b
Juan Herrera <juan.herrera@thoughtworks.com>
2024-04-06 04:03:48 -0300

    feat: measure recovery
    Added measure.py backup after hotfix

---
+++ src/measure.py
@@ -0,0 +1,38 @@
+import time
+
+def measure_elapsed_time(
+    start_time: str
+) -> float:
+    """
+    Calculates the elapsed time since the provided start time and returns it as a float.
+
+    Args:
+        start_time (float): The start time captured previously using time.time().
+
+    Returns:
+        float: The elapsed time in seconds.
+    """
+
+    end_time = time.time()
+    elapsed_time = end_time - float(start_time)
+    return elapsed_time  # Return the elapsed time for further use
+
+def print_elapsed_time(
+    elapsed_time: float
+) -> None:
+    """
+    Prints the elapsed time in a user-friendly format (seconds, minutes, or hours).
+
+    Args:
+        elapsed_time (float): The elapsed time in seconds.
+    """
+
+    time_unit = "seconds"
+    if elapsed_time >= 60:
+        elapsed_time /= 60
+        time_unit = "minutes"
+        if elapsed_time >= 60:
+            elapsed_time /= 60
+            time_unit = "hours"
+
+    print(f"Elapsed time: {elapsed_time:.2f} {time_unit}")
