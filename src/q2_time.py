c9058eb118f7947655c3330f26c77d4e883c54e3
Juan Herrera <juan.herrera@thoughtworks.com>
2024-04-06 11:20:13 -0300

    feat: challenge question 2 time completion
    Added time profiler import and decorator
    Added logic to process bigquery results from the queries.py query

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,23 @@
 from typing import List, Tuple
+import datetime
+from google.cloud import bigquery
+from processing import process_bigquery_results
+import line_profiler
 
-def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+@line_profiler.profile
+def q2_time(
+    client: bigquery.Client,
+    query: str
+) -> List[Tuple[str, int]]:
+    """
+    Executes a BigQuery SQL query and returns a list of tuples containing dates and strings extracted from the results.
+
+    Args:
+        client (bigquery.Client): A BigQuery client object.
+        query (str): The BigQuery SQL query to be executed.
+
+    Returns:
+        List[Tuple[datetime.date, str]]: A list of tuples where each tuple contains a datetime.date object and a string.
+    """
+
+    return process_bigquery_results(client, query)
